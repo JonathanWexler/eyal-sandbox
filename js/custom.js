5fbc5a22b3a8eac9c5ae513de231c73c9efde039
Jonathan Wexler <Jwexman@gmail.com>
2015-03-27 13:43:59 -0400

    card

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,19 @@
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=125569636000&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+$(document).ready(function() {  
+  
+//set opacity to 0.4 for all the images  
+//opacity = 1 - completely opaque  
+//opacity = 0 - invisible  
+  
+$('#card').css('opacity', 0.0);  
+$('#card').stop().fadeTo(4000, 1);  
+  
+// when hover over the selected image change the opacity to 1  
+$('#card').hover(  
+   function(){  
+      $(this).find('#card').stop().fadeTo('slow', 1);  
+   },  
+   function(){  
+      $(this).find('#card').stop().fadeTo('slow', 0.4);  
+   });  
+  
+}); 
